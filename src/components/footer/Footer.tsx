2b83aba7a1df388b21bad47ffebda839fc66c490
jay-rt <thapa.jayrt@gmail.com>
2023-08-07 21:30:17 -0400

    made some changes to navbar and finished footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,12 @@
 import "./footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>jayadmin</span>
+      <span>© Jay Dev Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
