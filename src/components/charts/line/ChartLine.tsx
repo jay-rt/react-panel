f7dc05e64fd05b551c0ff93decc8a2fbe3c857d4
jay-rt <thapa.jayrt@gmail.com>
2023-08-11 19:38:52 -0400

    created linechart components

---
+++ src/components/charts/line/ChartLine.tsx
@@ -0,0 +1,64 @@
+import "./chartline.scss";
+import { Link } from "react-router-dom";
+import { Line, LineChart, ResponsiveContainer, Tooltip } from "recharts";
+
+type Props = {
+  title: string;
+  icon: string;
+  number: number | string;
+  color: string;
+  dataKey: string;
+  percentage: number;
+  chartData: object[];
+};
+
+const ChartLine = (props: Props) => {
+  return (
+    <div className="linecard">
+      <div className="left">
+        <div className="title">
+          <img src={props.icon} alt="" />
+          <span>{props.title}</span>
+        </div>
+        <p>{props.number}</p>
+        <Link to="/" style={{ color: props.color }}>
+          View all
+        </Link>
+      </div>
+      <div className="right">
+        <div className="chart">
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart data={props.chartData}>
+              <Tooltip
+                contentStyle={{ background: "transparent", border: "none" }}
+                labelStyle={{ display: "none" }}
+                position={{ x: 10, y: 60 }}
+              />
+              <Line
+                type="monotone"
+                dataKey={props.dataKey}
+                stroke={props.color}
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+        <div className="desc">
+          <span
+            className={
+              props.percentage > 0
+                ? "percentage positive"
+                : "percentage negative"
+            }
+          >
+            {props.percentage}%
+          </span>
+          <span className="duration">this month</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChartLine;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,12 @@
 import "./home.scss";
-import TopDeals from "../../components/cards/topdeals/TopDeals";
+import TopDeals from "../../components/topdeals/TopDeals";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "./lineChartData";
+import ChartLine from "../../components/charts/line/ChartLine";
 
 const Home = () => {
   return (
@@ -7,11 +14,19 @@ const Home = () => {
       <div className="box box1">
         <TopDeals />
       </div>
-      <div className="box box2">Box2</div>
-      <div className="box box3">Box3</div>
+      <div className="box box2">
+        <ChartLine {...chartBoxUser} />
+      </div>
+      <div className="box box3">
+        <ChartLine {...chartBoxRevenue} />
+      </div>
       <div className="box box4">Box4</div>
-      <div className="box box5">Box5</div>
-      <div className="box box6">Box6</div>
+      <div className="box box5">
+        <ChartLine {...chartBoxProduct} />
+      </div>
+      <div className="box box6">
+        <ChartLine {...chartBoxConversion} />
+      </div>
       <div className="box box7">Box7</div>
       <div className="box box8">Box8</div>
       <div className="box box9">Box9</div>
