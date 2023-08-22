78fa96e5d3cbfc5fcdd4b9bf75d8c9494b45ccc8
jay-rt <thapa.jayrt@gmail.com>
2023-08-22 14:18:50 -0400

    disabled react dev tools in production

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./styles/global.scss";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
